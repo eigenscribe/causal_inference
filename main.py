520a88db29c6212cd45025283ab1090c00c198bb
Lauren Shriver <111261353+eigenscribe@users.noreply.github.com>
2024-03-22 11:39:10 -0400

    Create main.py
    idk what im doing

---
+++ main.py
@@ -0,0 +1,8 @@
+from bookstore_associations import *
+
+scm = BookSCM(random_seed=45) # instantiate SCM
+buy_book_a, buy_book_b = scm.sample(100) # sample 100 samples from SCM
+buy_book_a.shape, buy_book_b.shape # check whether the shapes are as expected
+
+proba_book_a_given_book_b = buy_book_a[buy_book_b].sum() / buy_book_a[buy_book_b].shape[0]
+print(f"Probability of buying book A given book B: {proba_book_a_given_book_b:0.3f}")
