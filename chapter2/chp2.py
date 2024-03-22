10bc8c2654a86167f1ef3d1eebf28d47292afcab
Lauren Shriver <111261353+eigenscribe@users.noreply.github.com>
2024-03-22 11:42:09 -0400

    Create chp2.py

---
+++ chapter2/chp2.py
@@ -0,0 +1,11 @@
+import numpy as np
+from scipy import stats
+from colors import bcolors as bc
+
+
+from bookstore_associations import *
+print()
+print()
+print(f"✨✨ Mean and Variance of B before vs. after Intervention on A ✨✨")
+print()
+from bookstore_interventions1 import *
