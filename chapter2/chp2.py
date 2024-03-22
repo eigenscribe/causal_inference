3c567e5e2acd07712c3130019b419d13d4a398d3
Lauren Shriver <111261353+eigenscribe@users.noreply.github.com>
2024-03-22 12:25:06 -0400

    Update chp2.py

--- chapter2/chp2.py
+++ chapter2/chp2.py
@@ -4,8 +4,13 @@ from colors import bcolors as bc
 
 
 from bookstore_associations import *
-print()
-print()
+scm = BookSCM(random_seed=45) # instantiate SCM
+  buy_book_a, buy_book_b = scm.sample(100) # sample 100 samples from SCM
+  buy_book_a.shape, buy_book_b.shape # check whether the shapes are as expected
+
+  proba_book_a_given_book_b = buy_book_a[buy_book_b].sum() / buy_book_a[buy_book_b].shape[0]
+  print(f"Probability of buying book A given book B: {proba_book_a_given_book_b:0.3f}\n\n")
+
 print(f"✨✨ Mean and Variance of B before vs. after Intervention on A ✨✨")
 print()
 from bookstore_interventions1 import *
